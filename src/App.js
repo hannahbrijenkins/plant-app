e1ddd4114f4716b22967c620cc9da7dd6d8282c2
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-12-20 22:05:30 -0500

    started creating form element, error with installing bootstrap to be worked out later

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      {/* Plant Name */}
+      <label>
+        Plant Name:
+        <input type="text" name="name" placeholder="Plant Name"/>
+      </label>
+      {/* Scientific Name */}
+      <label>
+        Scientific Name:
+        <input type="text" name="name" placeholder="Scientific Name"/>
+      </label>
+      {/* photos */}
+      <label>
+        Plant Photos:
+        <input type="file" name="name"/>
+      </label>
+      {/* Sun */}
+      <label>
+        Choose the amount of sun your plant needs.
+        <select>
+          <option value="full">Full</option>
+          <option value="bright">Bright</option>
+          <option value="partial">Partial</option>
+          <option value="indirect">Indirect</option>
+          <option value="shade">Shade</option>
+        </select>
+      </label>
+      {/* water */}
+      <label>
+        Choose the amount of water your plant needs.
+        <select>
+          <option value="frequently">Frequently</option>
+          <option value="infrequently">Infrequently</option>
+          <option value="rarely">Rarely</option>
+          <option value="moist">Keep Moist</option>
+          <option value="dry">Allow to Fully Dry</option>
+        </select>
+      </label>
+    </form>
   );
 }
 
